fac7f09f5fa9663d6ad506985de2466fd29604fb
jmsd9711 <joao.delgado.ifpr.2023@gmail.com>
2025-09-03 16:11:57 -0300

    Exercicio 03 09
    jogo de adivinhacao

---
+++ Exercicio0309/js/index.js
@@ -0,0 +1,19 @@
+const nAleatorio = Math.floor(Math.random() * 101);
+console.log(nAleatorio);
+
+var palpite, tentativas = 1
+
+palpite = parseInt(prompt(nAleatorio+"Digite um valor: "))
+
+while(palpite!=nAleatorio){
+    if(palpite>nAleatorio){
+        alert("Seu palpite é maior que o número sorteado")
+    }
+    if(palpite<nAleatorio){
+        alert("Seu palpite é menor que o número sorteado")
+    }
+    palpite = parseInt(prompt("Digite um novo valor: "))
+
+    tentativas++
+}
+alert("Você acertou o valor!! "+nAleatorio+" em "+tentativas+" Tentativas")

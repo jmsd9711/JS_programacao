110a6ee7761bf376e62c5bca371fdc4941c6c7cb
jmsd9711 <joao.delgado.ifpr.2023@gmail.com>
2025-09-10 15:48:22 -0300

    while para for
    while para for

--- Exercicio0309/js/index.js
+++ Exercicio0309/js/index.js
@@ -1,19 +1,16 @@
 const nAleatorio = Math.floor(Math.random() * 101);
 console.log(nAleatorio);
 
-var palpite, tentativas = 1
+let palpite, tentativas = 0;
 
-palpite = parseInt(prompt("Digite um valor: "))
+for (tentativas = 1; palpite !== nAleatorio; tentativas++) {
+  palpite = parseInt(prompt("Digite um valor: "));
 
-while(palpite!=nAleatorio){
-    if(palpite>nAleatorio){
-        alert("Seu palpite é maior que o número sorteado")
-    }
-    if(palpite<nAleatorio){
-        alert("Seu palpite é menor que o número sorteado")
-    }
-    palpite = parseInt(prompt("Digite um novo valor: "))
-
-    tentativas++
+  if (palpite > nAleatorio) {
+    alert("Seu palpite é maior que o número sorteado");
+  } else if (palpite < nAleatorio) {
+    alert("Seu palpite é menor que o número sorteado");
+  } else {
+    alert("Você acertou o valor!! " + nAleatorio + " em " + tentativas + " Tentativas");
+  }
 }
-alert("Você acertou o valor!! "+nAleatorio+" em "+tentativas+" Tentativas")
